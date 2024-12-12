502031a15790e8c2a2948c976103d52af53eb41a
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-12-12 10:50:43 +0000

    Improve config error handling

--- config_helper.py
+++ config_helper.py
@@ -1,9 +1,13 @@
 import configparser
 
+
 class ConfigHelper:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read("config.ini")
 
     def __getitem__(self, key):
+        if key not in self.config:
+            raise KeyError(f"Key '{key}' not found in config file")
+
         return self.config[key]
