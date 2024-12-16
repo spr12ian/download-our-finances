0efc4375b94680c8e435dc2f7f33f5b1e1669028
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-12-16 14:51:05 +0000

    Calculations next

--- hmrc_reports_our_finances.py
+++ hmrc_reports_our_finances.py
@@ -84,7 +84,7 @@ class HMRC:
     def get_answers(self):
         answers = []
         for question, page, section, box, method_name in self.get_questions():
-            if box=="1":
+            if box == "1":
                 answers = self.append_section(answers, section)
 
             answer = self.call_method(method_name)
@@ -223,6 +223,156 @@ class HMRC:
 
         return how_many
 
+    def get_lump_sum_pension___available_lifetime_allowance(self):
+        return "Not applicable"
+
+    def get_non_lump_sum_pension___available_lifetime_allowance(self):
+        return "Not applicable"
+
+    def get_lifetime_allowance_tax_paid_by_your_pension_scheme(self):
+        return "Not applicable"
+
+    def get_amount_saved_to_pension___available_lifetime_allowance(self):
+        return "Not applicable"
+
+    def get_annual_allowance_tax_paid(self):
+        return "Not applicable"
+
+    def get_amount_subject_to_overseas_transfer_charge(self):
+        return "Not applicable"
+
+    def get_tax_paid_on_overseas_transfer_charge(self):
+        return "Not applicable"
+
+    def get_pension_scheme_tax_reference_number(self):
+        return "Not applicable"
+
+    def get_unauthorised_pension_payment_not_surcharged_(self):
+        return "Not applicable"
+
+    def get_unauthorised_pension_payment_surcharged_(self):
+        return "Not applicable"
+
+    def get_foreign_tax_paid_on_an_unauthorised_payment(self):
+        return "Not applicable"
+
+    def get_taxable_short_service_refund__overseas_only_(self):
+        return "Not applicable"
+
+    def get_box_17_is_not_in_use(self):
+        return "Not applicable"
+
+    def get_foreign_tax_paid_on_box_16(self):
+        return "Not applicable"
+
+    def get_tax_avoidance_scheme_reference_number(self):
+        return "Not applicable"
+
+    def get_tax_year_expected_advantage_arises(self):
+        return "Not applicable"
+
+    def get_description_of_business(self):
+        return "Not applicable"
+
+    def get_postcode_of_your_business_address(self):
+        return "Not applicable"
+
+    def get_have_business_details_changed__yes_no_(self):
+        return "Not applicable"
+
+    def get_are_you_a_foster_carer(self):
+        return "Not applicable"
+
+    def get_business_start_date__in_this_tax_year_(self):
+        return "Not applicable"
+
+    def get_business_end_date__in_this_tax_year_(self):
+        return "Not applicable"
+
+    def get_date_the_books_are_made_up_to(self):
+        return "Not applicable"
+
+    def get_cash_basis__yes_no_(self):
+        return "Not applicable"
+
+    def get_turnover(self):
+        return "Not applicable"
+
+    def get_any_other_business_income_not_included_in_box_9(self):
+        return "Not applicable"
+
+    def get_trading_income_allowance(self):
+        return "Not applicable"
+
+    def get_turnover_was_below__85k__total_expenses_in_box_20(self):
+        return "See box 20"
+
+    def get_total_allowable_expenses(self):
+        return "Not applicable"
+
+    def get_net_profit(self):
+        return "Not applicable"
+
+    def get_net_loss(self):
+        return "Not applicable"
+
+    def get_annual_investment_allowance(self):
+        return "Not applicable"
+
+    def get_allowance_for_small_balance_of_unrelieved_expenditure(self):
+        return "Not applicable"
+
+    def get_zero_emission_car_allowance(self):
+        return "Not applicable"
+
+    def get_other_capital_allowances(self):
+        return "Not applicable"
+
+    def get_the_structures_and_buildings_allowance(self):
+        return "Not applicable"
+
+    def get_freeport___investment_zones_allowance(self):
+        return "Not applicable"
+
+    def get_total_balancing_charges(self):
+        return "Not applicable"
+
+    def get_goods_services_for_your_own_use(self):
+        return "Not applicable"
+
+    def get_net_business_profit_for_tax_purposes(self):
+        return "Not applicable"
+
+    def get_loss_brought_forward(self):
+        return "Not applicable"
+
+    def get_other_business_income_not_included_in_boxes_9_or_10(self):
+        return "Not applicable"
+
+    def get_total_taxable_profits_from_this_business(self):
+        return "Not applicable"
+
+    def get_net_business_loss_for_tax_purposes(self):
+        return "Not applicable"
+
+    def get_loss_from_this_tax_year_set_off_against_other_income(self):
+        return "Not applicable"
+
+    def get_loss_to_be_carried_back_to_previous_years(self):
+        return "Not applicable"
+
+    def get_total_loss_to_carry_forward(self):
+        return "Not applicable"
+
+    def get_total_profits____6_725_voluntary_class_2_nics__yes_no_(self):
+        return "Not applicable"
+
+    def get_are_you_exempt_from_paying_class_4_nics(self):
+        return "Not applicable"
+
+    def get_total_construction_industry_scheme__cis__deductions(self):
+        return "Not applicable"
+
     def get_how_many_partnerships(self):
         return 0
 
@@ -292,6 +442,318 @@ class HMRC:
         return self.get_year_category_total(
             tax_year, f"HMRC {person_code} INC Taxed UK interest"
         )
+
+    def get_payments_to_annuity_tax_relief_not_claimed(self):
+        return "Not applicable"
+
+    def get_payments_to_employers_scheme_where_no_tax_deducted(self):
+        return "Not applicable"
+
+    def get_payments_to_overseas_pension_scheme(self):
+        return "Not applicable"
+
+    def get_gift_aid_payments(self):
+        return "Not applicable"
+
+    def get_total_of_any__one_off__payments_in_box_5(self):
+        return "Not applicable"
+
+    def get_gift_aid_payments_from_previous_tax_year_(self):
+        return "Not applicable"
+
+    def get_gift_aid_payments_after_end_of_tax_year(self):
+        return "Not applicable"
+
+    def get_value_of_shares_dividends_gifted_to_charities(self):
+        return "Not applicable"
+
+    def get_value_of_land_or_buildings_gifted_to_charities(self):
+        return "Not applicable"
+
+    def get_value_gifted_to_non_uk_charities_in_box_9_and_10(self):
+        return "Not applicable"
+
+    def get_for_how_many_children__cb_(self):
+        return "Not applicable"
+
+    def get_date_cb_stopped(self):
+        return "Not applicable"
+
+    def get_your_spouse_s_first_name(self):
+        return "Not applicable"
+
+    def get_your_spouse_s_last_name(self):
+        return "Not applicable"
+
+    def get_your_spouse_s_national_insurance_number(self):
+        return "Not applicable"
+
+    def get_your_spouse_s_date_of_birth(self):
+        return "Not applicable"
+
+    def get_marriage_date(self):
+        return "Not applicable"
+
+    def get_refunded_or_off_set_income_tax(self):
+        return "Not applicable"
+
+    def get_use_paye_for_small_amount_payments__yes_no_(self):
+        return "Not applicable"
+
+    def get_use_paye_for_tax_on_savings__yes_no_(self):
+        return "Not applicable"
+
+    def get_bank_name(self):
+        return "Not applicable"
+
+    def get_bank_account_holder(self):
+        return "Not applicable"
+
+    def get_branch_sort_code(self):
+        return "Not applicable"
+
+    def get_bank_account_number(self):
+        return "Not applicable"
+
+    def get_building_society_reference_number(self):
+        return "Not applicable"
+
+    def get_cheque__yes_no_(self):
+        return "Not applicable"
+
+    def get_did_you_put_a_nominee_s_name_in_box_5__yes_no_(self):
+        return "Not applicable"
+
+    def get_is_your_nominee_your_tax_advisor__yes_no_(self):
+        return "Not applicable"
+
+    def get_nominee_s_address(self):
+        return "Not applicable"
+
+    def get_nominee_s_postcode(self):
+        return "Not applicable"
+
+    def get_signature_to_authorise_nominee(self):
+        return "Not applicable"
+
+    def get_tax_advisor_s_name(self):
+        return "Not applicable"
+
+    def get_tax_advisor_s_phone_number(self):
+        return "Not applicable"
+
+    def get_tax_advisor_s_address_and_postcode(self):
+        return "Not applicable"
+
+    def get_tax_advisor_s_reference(self):
+        return "Not applicable"
+
+    def get_any_other_information(self):
+        return "Not applicable"
+
+    def get_provisional_figures__yes_no_(self):
+        return "Not applicable"
+
+    def get_supplementary_pages_enclodsed__yes_no_(self):
+        return "Not applicable"
+
+    def get_declaration_signature(self):
+        return "Not applicable"
+
+    def get_declaration_date(self):
+        return "Not applicable"
+
+    def get_signing_capacity_if_signing_for_someone_else(self):
+        return "Not applicable"
+
+    def get_name_of_the_peson_you_ve_signed_for(self):
+        return "Not applicable"
+
+    def get_if_you_filled_in_boxes_23_and_24_enter_your_name(self):
+        return "Not applicable"
+
+    def get_and_your_address(self):
+        return "Not applicable"
+
+    def get_gilt_interest_after_tax_taken_off(self):
+        return "Not applicable"
+
+    def get_tax_taken_off(self):
+        return "Not applicable"
+
+    def get_gross_amount_before_tax(self):
+        return "Not applicable"
+
+    def get_uk_gains_where_tax_was_treated_as_paid(self):
+        return "Not applicable"
+
+    def get_number_of_years_policy_was_last_held_or_received_gain(self):
+        return "Not applicable"
+
+    def get_uk_gains_where_tax_was_not_treated_as_paid(self):
+        return "Not applicable"
+
+    def get_how_many_years_policy_was_last_held_or_received_gain(self):
+        return "Not applicable"
+
+    def get_gains_from_voided_isas(self):
+        return "Not applicable"
+
+    def get_years_voided_isas_held(self):
+        return "Not applicable"
+
+    def get_tax_taken_off_gain_in_box_8(self):
+        return "Not applicable"
+
+    def get_deficiency_relief(self):
+        return "Not applicable"
+
+    def get_stock_dividends(self):
+        return "Not applicable"
+
+    def get_bonus_issues(self):
+        return "Not applicable"
+
+    def get_loans_written_off(self):
+        return "Not applicable"
+
+    def get_post_cessation_receipts_amount(self):
+        return "Not applicable"
+
+    def get_earlier_year_tax_year_to_be_taxed(self):
+        return "Not applicable"
+
+    def get_share_schemes_taxable_amount(self):
+        return "Not applicable"
+
+    def get_box_2_is_not_is_use(self):
+        return "Not applicable"
+
+    def get_taxable_lump_sums(self):
+        return "Not applicable"
+
+    def get_lump_sums_employer_financed_retirement_benefits_scheme(self):
+        return "Not applicable"
+
+    def get_redundancy_payments(self):
+        return "Not applicable"
+
+    def get_tax_taken_off_boxes_3_to_5(self):
+        return "Not applicable"
+
+    def get_is_box_6_blank_as_tax_inc__in_box_2_of__employment_(self):
+        return "Not applicable"
+
+    def get_exemptions_for_amounts_entered_in_box_4(self):
+        return "Not applicable"
+
+    def get_compensation_and_lump_sums_up_to__30_000_exemption(self):
+        return "Not applicable"
+
+    def get_disability_and_foreign_service_deduction(self):
+        return "Not applicable"
+
+    def get_seafarers__earnings_deduction(self):
+        return "Not applicable"
+
+    def get_foreign_earnings_not_taxable_in_the_uk(self):
+        return "Not applicable"
+
+    def get_foreign_tax_where_tax_credit_not_claimed(self):
+        return "Not applicable"
+
+    def get_exempt_overseas_employee_contributions(self):
+        return "Not applicable"
+
+    def get_uk_patent_royalty_payments_made(self):
+        return "Not applicable"
+
+    def get_subscriptions_for_venture_capital_trust_shares(self):
+        return "Not applicable"
+
+    def get_subscriptions_for_enterprise_investment_scheme_shares(self):
+        return "Not applicable"
+
+    def get_community_investment_tax_relief(self):
+        return "Not applicable"
+
+    def get_annual_payments_made(self):
+        return "Not applicable"
+
+    def get_qualifying_loan_interest_payable_in_the_year(self):
+        return "Not applicable"
+
+    def get_post_cessation_trade_relief_and_certain_other_losses(self):
+        return "Not applicable"
+
+    def get_pre_incorporation_losses(self):
+        return "Not applicable"
+
+    def get_maintenance_payments(self):
+        return "Not applicable"
+
+    def get_payments_to_a_trade_union_for_death_benefits(self):
+        return "Not applicable"
+
+    def get_relief_claimed_on_a_qualifying_distribution(self):
+        return "Not applicable"
+
+    def get_shares_under_the_seed_enterprise_investment_scheme(self):
+        return "Not applicable"
+
+    def get_box_11_is_not_in_use(self):
+        return "Not applicable"
+
+    def get_non_deductible_loan_interest_letting_pship_investments(self):
+        return "Not applicable"
+
+    def get_not_applicable(self):
+        return "Not applicable"
+
+    def get_earlier_years__losses(self):
+        return "Not applicable"
+
+    def get_total_unused_losses_carried_forward(self):
+        return "Not applicable"
+
+    def get_relief_now_for_following_year_trade_losses(self):
+        return "Not applicable"
+
+    def get_amount_in_box_3_not_subject_to_income_tax_relief_limit(self):
+        return "Not applicable"
+
+    def get_tax_year_for_which_you_re_claiming_relief_in_box_3_(self):
+        return "Not applicable"
+
+    def get_amount_of_payroll_giving(self):
+        return "Not applicable"
+
+    def get_gift_aid_payments_to_non_uk_charities_in_box_6(self):
+        return "Not applicable"
+
+    def get_registered_blind_(self):
+        return False
+
+    def get_local_authority_or_other_register(self):
+        return "Not applicable"
+
+    def get_do_you_want_spouse_s_surplus_allowance(self):
+        return "Not applicable"
+
+    def get_should_your_spouse_get_your_surplus_allowance(self):
+        return "Not applicable"
+
+    def get_have_you_received_student_loans_company_notification(self):
+        return False
+
+    def get_any_employer_deducted_student_loan(self):
+        return "Not applicable"
+
+    def get_any_employer_deducted_post_grad_loan(self):
+        return "Not applicable"
+
+    def get_how_much_child_benefit__cb__received(self):
+        return 0.0
 
     def get_title(self):
         full_utr = self.get_full_utr()
