4db797a8de55182deb7a89fe63c4eaf4a1c20914
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-02-07 09:00:07 +0000

    Separated raw sqls

--- execute_sqls.py
+++ execute_sqls.py
@@ -1,51 +1,10 @@
-from cls_helper_log import LogHelper
 from cls_helper_sqlalchemy import SQLAlchemyHelper
 from models import BankAccounts, Transactions
-from sqlalchemy import create_engine, func, not_, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func, not_, text
 import utility_functions as uf
 
-DATABASE_URL = "sqlite:///our_finances.sqlite"
-
-# Create an engine
-#engine = create_engine(DATABASE_URL, echo=True)
-engine = create_engine(DATABASE_URL)
-
-queries = [
-    """
-SELECT strftime('%Y-%m', t.date) as month, 
-SUM(credit) as money_in, 
-SUM(debit) as money_out, 
-SUM(credit - debit) as net_amount
-FROM bank_accounts b 
-JOIN transactions t ON b.key = t.key
-WHERE b."our_money" = "TRUE"
-AND t.description NOT LIKE "X%"
-GROUP BY month
-ORDER BY month
-    """,
-    """
-SELECT t.key, SUM(credit) as money_in, SUM(debit) as money_out, SUM(credit - debit)
-FROM bank_accounts b JOIN transactions t
-ON b.key = t.key
-WHERE "our_money"="TRUE"
-AND t.date BETWEEN "2025-01-01" AND "2025-01-31"
-AND t.description NOT LIKE "X%"
-GROUP BY t.key
-    """,
-]
-# Execute the query
-# with engine.connect() as connection:
-#     for query in queries:
-#         print(query)
-#         result = connection.execute(text(query))
-#         for row in result:
-#             print(row)
-
-
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+sql = SQLAlchemyHelper()
+session = sql.get_session()
 
 # Perform the query
 results = (
@@ -59,7 +18,7 @@ results = (
     .filter(
         BankAccounts.our_money == "TRUE",
         not_(Transactions.description.like("X%")),
-        Transactions.date.between("2025-02-01", "2025-02-28"),
+        Transactions.date.between("2025-01-01", "2025-01-31"),
     )
     .group_by(func.strftime("%Y-%m", Transactions.date))
     .order_by("month")
@@ -90,23 +49,19 @@ results = (
     .filter(
         BankAccounts.our_money == "TRUE",
         not_(Transactions.description.like("X%")),
-        Transactions.date.between("2025-02-01", "2025-02-28"),
+        Transactions.date.between("2025-01-01", "2025-01-31"),
     )
     .order_by("d")
     .all()
 )
 
-if len(results):    
-    print(
-        "Date           Credit      Debit        Net Description"
-    )
+if len(results):
+    print("Date           Credit      Debit        Net Description")
     # Print the results
     for result in results:
-        date=result.d
-        plus=uf.format_as_gbp(result.plus, 11)
-        minus=uf.format_as_gbp(result.minus, 11)
-        net=uf.format_as_gbp(result.net, 11)
-        description=result.description
-        print(
-            f"{date}{plus}{minus}{net} {description}"
-        )
+        date = result.d
+        plus = uf.format_as_gbp(result.plus, 11)
+        minus = uf.format_as_gbp(result.minus, 11)
+        net = uf.format_as_gbp(result.net, 11)
+        description = result.description
+        print(f"{date}{plus}{minus}{net} {description}")
---
+++ queries.sql
@@ -0,0 +1,21 @@
+-- Query 1: Monthly summary
+SELECT strftime('%Y-%m', t.date) as month, 
+SUM(credit) as money_in, 
+SUM(debit) as money_out, 
+SUM(credit - debit) as net_amount
+FROM bank_accounts b 
+JOIN transactions t ON b.key = t.key
+WHERE b."our_money" = "TRUE"
+AND t.description NOT LIKE "X%"
+GROUP BY month
+ORDER BY month
+;
+-- Query 2: 2025-01 by bank account
+SELECT t.key, SUM(credit) as money_in, SUM(debit) as money_out, SUM(credit - debit)
+FROM bank_accounts b JOIN transactions t
+ON b.key = t.key
+WHERE "our_money"="TRUE"
+AND t.date BETWEEN "2025-01-01" AND "2025-01-31"
+AND t.description NOT LIKE "X%"
+GROUP BY t.key
+;
