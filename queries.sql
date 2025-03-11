72543ecb4aba18d7dc9a7ac7031651c83ba2188e
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-03-11 16:43:00 +0000

    xfers by key

--- queries.sql
+++ queries.sql
@@ -18,4 +18,11 @@ WHERE "our_money"="TRUE"
 AND t.date BETWEEN "2025-01-01" AND "2025-01-31"
 AND t.description NOT LIKE "X%"
 GROUP BY t.key
+;-- Query 3: xfers by key
+SELECT key, category, COUNT(key) as how_many
+FROM transactions
+WHERE description = "XFER"
+AND tax_year = "2024 to 2025"
+GROUP BY key
+ORDER BY how_many DESC
 ;
