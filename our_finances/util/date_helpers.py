d847bfab51b51ef16e947567a64f8084e08ddcc7
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-08 14:49:06 +0100

    Preparing to delete the repository

--- our_finances/util/date_helpers.py
+++
@@ -1,5 +0,0 @@
-from our_finances.classes.date_time_helper import DateTimeHelper
-
-
-def UK_to_ISO(date_str: str) -> str:
-    return DateTimeHelper().UK_to_ISO(date_str)
--- test_date_time.py
+++
@@ -1,3 +0,0 @@
-from helper_date_time import DateTimeHelper
-
-print(DateTimeHelper().get_date_today())
